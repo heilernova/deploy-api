9645e9c242e204f670f2aaaf86cf97bd6d902e3c
Heiler Nova <heilernova@outlook.com>
2024-10-23 14:06:22 -0500

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { ControllersModule } from './controllers/controllers.module';
+import { ModelsModule } from './models/models.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, ControllersModule, ModelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
