c1298d8d059b81504d18849e592aa46fe513cd13
Heiler Nova <heilernova@outlook.com>
2024-10-23 14:02:14 -0500

    Fix: importacion sqlite3

--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import sqlite3 from 'sqlite3'
-import { open, Database } from 'sqlite'
+import * as sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
 @Injectable()
 export class DatabaseService {
-    private _con: Database<sqlite3.Database, sqlite3.Statement> | null = null;
     constructor(){
         this.init();
     }
@@ -17,16 +16,10 @@ export class DatabaseService {
     }
 
     public async getConnection(){
-        if (this._con){
-            return this._con;
-        }
-
         const db = await open({
             filename: './app.db',
             driver: sqlite3.Database
         })
-
-        this._con = db;
         return db;
     }
 }
