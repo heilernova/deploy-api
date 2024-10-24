1d9775ddcdc551dda8b8e55d59373b48841747a9
Heiler Nova <heilernova@outlook.com>
2024-10-24 09:06:57 -0500

    Update

--- src/models/tokens/tokens.service.ts
+++ src/models/tokens/tokens.service.ts
@@ -10,7 +10,6 @@ export class TokensService {
     async create(data: { userId: string, type: "web" | "cli", hostname: string, ip: string, device: "desktop" | "mobile" | "tablet"}){
         const conn = await this._db.getConnection();
 
-        
         if (data.type == "web"){
             let tokens = await conn.all("select * from users_tokens where user_id = ? order by created_at", [data.userId]);
             if (tokens.length >= 3){
